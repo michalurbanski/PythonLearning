2194da6a664da5d4dd59075b100830c0a7982955
michal_urbanski <michal_urbanski@windowslive.com>
2016-04-05 22:50:18 +0200

    Adds operations on collectionns - tuple and list

--- collections.py
+++ collections.py
@@ -1,4 +1,4 @@
-# Tuples
+# Tuples - can't be modified
 
 tuple = ('first', 'second', 'third')
 print(tuple)
@@ -15,11 +15,35 @@ for i in range(len(tuple)): # Note colon at the end of 'for' statement
 
 try:
   tuple[0] = 'new element'
-except:
-  print("Tuple can't be modified!")
+except Exception as e:
+  print("Tuple can't be modified! message: %s" % e)
 
 # Store different elements in tuple
 print("\nTuple can hold different type of elements")
 multiPurposeTuple = ('first', 1)
 print(multiPurposeTuple)
 print(type(multiPurposeTuple[1]))
+
+# List - can be modified
+print("\nLists:")
+breakfast = ["egg", "tomato"]
+
+# Data can be also printed like in foreach loop
+breakfast[0] = "toast"
+for i in breakfast:
+  print i
+
+# Add elements to list
+breakfast.append("bacon")
+print(breakfast)
+
+# Slicing collection
+firstTwoListElements = breakfast[0:2] # start position:end position
+print(firstTwoListElements)
+
+# Take last element from list
+if breakfast.pop() == "bacon":
+  print("Last element taken from list")
+  print(breakfast)
+
+
